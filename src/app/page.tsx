7f00af204658d6e3ce059c13e0292ea94b0d6aa0
Filipe Mallmann Siota <filipesiota@gmail.com>
2024-07-17 01:47:01 -0300

    FEAT: Build initial form template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,118 +1,169 @@
-import Image from 'next/image';
-
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-					Get started by editing&nbsp;
-					<code className='font-mono font-bold'>
-						src/app/page.tsx
-					</code>
+		<main className="flex flex-col items-center w-full max-w-3xl mx-auto py-8 px-4">
+			<div className="flex flex-col gap-2 items-center">
+				<h2 className="sm:text-6xl text-4xl font-bold tracking-tight text-gray-900">
+					SoundvetX
+				</h2>
+				<p className="text-base leading-6 text-gray-900">
+					Radiologia em animais de companhia e pets exóticos
 				</p>
-				<div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
-					<a
-						className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-						href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						By{' '}
-						<Image
-							src='/vercel.svg'
-							alt='Vercel Logo'
-							className='dark:invert'
-							width={100}
-							height={24}
-							priority
-						/>
-					</a>
+			</div>
+
+			<form className="w-full mt-12">
+				<h2 className="text-2xl font-semibold leading-7 text-gray-900">
+					Dados do Requerente
+				</h2>
+
+				<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+					<div className="sm:col-span-3">
+						<label
+							htmlFor="first-name"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Clínica Veterinária
+						</label>
+						<div className="mt-2">
+							<input
+								id="first-name"
+								name="first-name"
+								type="text"
+								autoComplete="given-name"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-3">
+						<label
+							htmlFor="last-name"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Last name
+						</label>
+						<div className="mt-2">
+							<input
+								id="last-name"
+								name="last-name"
+								type="text"
+								autoComplete="family-name"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-4">
+						<label
+							htmlFor="email"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Email address
+						</label>
+						<div className="mt-2">
+							<input
+								id="email"
+								name="email"
+								type="email"
+								autoComplete="email"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-3">
+						<label
+							htmlFor="country"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Country
+						</label>
+						<div className="mt-2">
+							<select
+								id="country"
+								name="country"
+								autoComplete="country-name"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+							>
+								<option>United States</option>
+								<option>Canada</option>
+								<option>Mexico</option>
+							</select>
+						</div>
+					</div>
+
+					<div className="col-span-full">
+						<label
+							htmlFor="street-address"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Street address
+						</label>
+						<div className="mt-2">
+							<input
+								id="street-address"
+								name="street-address"
+								type="text"
+								autoComplete="street-address"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-2 sm:col-start-1">
+						<label
+							htmlFor="city"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							City
+						</label>
+						<div className="mt-2">
+							<input
+								id="city"
+								name="city"
+								type="text"
+								autoComplete="address-level2"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-2">
+						<label
+							htmlFor="region"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							State / Province
+						</label>
+						<div className="mt-2">
+							<input
+								id="region"
+								name="region"
+								type="text"
+								autoComplete="address-level1"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+
+					<div className="sm:col-span-2">
+						<label
+							htmlFor="postal-code"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							ZIP / Postal code
+						</label>
+						<div className="mt-2">
+							<input
+								id="postal-code"
+								name="postal-code"
+								type="text"
+								autoComplete="postal-code"
+								className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
 				</div>
-			</div>
-
-			<div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-				<Image
-					className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-					src='/next.svg'
-					alt='Next.js Logo'
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
-
-			<div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
-				<a
-					href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<h2 className='mb-3 text-2xl font-semibold'>
-						Docs{' '}
-						<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-							-&gt;
-						</span>
-					</h2>
-					<p className='m-0 max-w-[30ch] text-sm opacity-50'>
-						Find in-depth information about Next.js features and
-						API.
-					</p>
-				</a>
-
-				<a
-					href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<h2 className='mb-3 text-2xl font-semibold'>
-						Learn{' '}
-						<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-							-&gt;
-						</span>
-					</h2>
-					<p className='m-0 max-w-[30ch] text-sm opacity-50'>
-						Learn about Next.js in an interactive course
-						with&nbsp;quizzes!
-					</p>
-				</a>
-
-				<a
-					href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<h2 className='mb-3 text-2xl font-semibold'>
-						Templates{' '}
-						<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-							-&gt;
-						</span>
-					</h2>
-					<p className='m-0 max-w-[30ch] text-sm opacity-50'>
-						Explore starter templates for Next.js.
-					</p>
-				</a>
-
-				<a
-					href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<h2 className='mb-3 text-2xl font-semibold'>
-						Deploy{' '}
-						<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-							-&gt;
-						</span>
-					</h2>
-					<p className='m-0 max-w-[30ch] text-balance text-sm opacity-50'>
-						Instantly deploy your Next.js site to a shareable URL
-						with Vercel.
-					</p>
-				</a>
-			</div>
+			</form>
 		</main>
 	);
 }
