401cbc1ec4b7b80c1dfa1c9c8656eb1d91930220
Filipe Mallmann Siota <filipesiota@gmail.com>
2024-07-24 02:09:29 -0300

    FIX: Add chromium to environment

--- src/utils/generate-pdf.ts
+++ src/utils/generate-pdf.ts
@@ -1,28 +1,53 @@
-import puppeteer from "puppeteer";
 import path from "path";
+import puppeteer from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+
 import { ComboReturn } from "@/@types/combo-return";
 import { RequestError } from "@/@types/request-response";
 import { readFileSync } from "fs";
 
 export async function generatePDF(): Promise<ComboReturn<string, RequestError>> {
-	const browser = await puppeteer.launch({ headless: true });
+	let browser = null;
+
+	if (process.env.NODE_ENV === "development") {
+		browser = await puppeteer.launch({
+			args: ["--no-sandbox", "--disable-setuid-sandbox"],
+			headless: true
+		});
+	} else if (process.env.NODE_ENV === "production") {
+		browser = await puppeteerCore.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath(),
+			headless: chromium.headless
+		});
+	} else {
+        return {
+            data: null,
+            error: {
+                message: "Invalid environment"
+            }
+        };
+    }
+
 	const page = await browser.newPage();
 	const absolutePath = path.resolve("src/templates/report.html");
 	await page.goto(`file://${absolutePath}`, { waitUntil: "networkidle0" });
 	await page.pdf({ path: path.resolve("public/report.pdf"), format: "A4" });
 	await browser.close();
 
-    if (readFileSync(path.resolve("public/report.pdf")).length === 0) {
-        return {
-            data: null,
-            error: {
-                message: "Failed to generate PDF"
-            }
-        };
-    }
+	if (readFileSync(path.resolve("public/report.pdf")).length === 0) {
+		return {
+			data: null,
+			error: {
+				message: "Failed to generate PDF"
+			}
+		};
+	}
 
-    return {
-        data: `${process.env.PUBLIC_URL}/report.pdf`,
-        error: null
-    };
+	return {
+		data: `${process.env.PUBLIC_URL}/report.pdf`,
+		error: null
+	};
 }
