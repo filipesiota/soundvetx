a1c057cfe3c4999f9a909118c4acd227af97ac10
Filipe Mallmann Siota <filipesiota@gmail.com>
2024-07-31 01:54:21 -0300

    TEST: Testing PDF generation part. 2

--- src/utils/generate-pdf.ts
+++ src/utils/generate-pdf.ts
@@ -1,21 +1,21 @@
-import * as Puppeteer from "puppeteer";
-import Chromium from "@sparticuz/chromium";
+import * as puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 import { ComboReturn } from "@/@types/combo-return";
 import { RequestError } from "@/@types/request-response";
 import { readFileSync } from "fs";
 import path from "path";
 
-async function getBrowser(): Promise<Puppeteer.Browser> {
-	if (process.env.VERCEL_ENV === "production") {
-		return Puppeteer.launch({
-			args: Chromium.args,
-			defaultViewport: Chromium.defaultViewport,
-			executablePath: await Chromium.executablePath(),
-			headless: Chromium.headless
+async function getBrowser(): Promise<puppeteer.Browser> {
+	if (process.env.NODE_ENV === "production") {
+		return puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath(),
+			headless: chromium.headless
 		});
 	}
 
-	return Puppeteer.launch({
+	return puppeteer.launch({
 		headless: true
 	});
 }
@@ -24,7 +24,9 @@ export async function generatePDF(): Promise<ComboReturn<Blob, RequestError>> {
 	try {
 		const browser = await getBrowser();
 		const page = await browser.newPage();
-		await page.goto(`${process.env.VERCEL_URL}/report.html`, { waitUntil: "networkidle0" });
+		const templatePath = path.join(process.cwd(), "src", "templates", "report.html");
+		const templateContent = readFileSync(templatePath, "utf8");
+		await page.setContent(templateContent, { waitUntil: "networkidle0" });
 		const pdfBuffer = await page.pdf({ format: "A4" });
 		await browser.close();
 
@@ -34,7 +36,7 @@ export async function generatePDF(): Promise<ComboReturn<Blob, RequestError>> {
 		};
 	} catch (error: any) {
 		console.log(error);
-
+		
 		return {
 			data: null,
 			error: {
